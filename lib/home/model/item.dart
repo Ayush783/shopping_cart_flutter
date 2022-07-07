975d0f0bd9354152f37fab195910c52208265581
Ayush783 <ayushc45xxx@gmail.com>
2022-07-07 14:43:35 +0530

    price field added

--- lib/home/model/item.dart
+++ lib/home/model/item.dart
@@ -4,24 +4,27 @@ class Item extends Equatable {
   final String name;
   final String img;
   final String desc;
+  final double price;
 
   const Item({
     required this.name,
     required this.img,
     required this.desc,
+    required this.price,
   });
 
   Item.fromJson(Map<String, dynamic> json)
       : name = json['name'],
         img = 'assets/dummy/${json['img']}',
-        desc = json['desc'];
+        desc = json['desc'],
+        price = json['price'];
 
   Item copyWith(Item i) {
-    return Item(name: i.name, img: i.img, desc: i.desc);
+    return Item(name: i.name, img: i.img, desc: i.desc, price: i.price);
   }
 
   @override
-  List<Object?> get props => [name, img, desc];
+  List<Object?> get props => [name, img, desc, price];
 }
 
 class ItemResponse {
